f3e7f7ce838a5deef5d3fc94351296b82ce65522
Javier Armando Méndez Pérez <55966989+JavierMendez-262@users.noreply.github.com>
2020-04-17 18:45:31 -0700

    Add files via upload

---
+++ SS_BDLocal/src/java/recursos/RecursoExpediente.java
@@ -0,0 +1,57 @@
+/*
+ * RecursoExpediente.java
+ * 
+ * Creado en Abril 17, 2020. 18:01.
+ */
+package recursos;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import objetosnegocio.Expediente;
+
+/**
+ * REST Web Service
+ *
+ * @author JavierMëndez & EnriqueMendoza
+ */
+@Path("expediente")
+public class RecursoExpediente {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of RecursoExpediente
+     */
+    public RecursoExpediente() {
+    }
+
+    /**
+     * Retrieves representation of an instance of recursos.RecursoExpediente
+     * @return an instance of objetosnegocio.Expediente
+     */
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getJson(@PathParam("id")String id) {
+        return Response.status(200).entity(new Expediente("Javier Méndez", "181816", "21/02/99", "Hernesto Valenzuela", "Estudiante", "[Texto]")).build();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of RecursoExpediente
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response putJson(Expediente content) {
+        return Response.status(200).build();
+    }
+}
