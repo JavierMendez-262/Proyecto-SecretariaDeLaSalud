be6dee97fab06199e285c653c8b77733eec66ad7
emendoza-216 <33888244+emendoza-216@users.noreply.github.com>
2020-04-17 19:56:20 -0700

    Add files via upload

---
+++ SS_PCMedica/src/control/Control.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import java.net.URI;
+import objetosnegocio.Expediente;
+import org.glassfish.tyrus.client.ClientManager;
+import rest.RecursoExpediente_Client;
+import websockets.ClientEndpointAnnotated;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Control {
+
+    private final String URI = "ws://localhost:8080/SS_BDLocal/consultaExpediente";
+    private RecursoExpediente_Client rec;
+    private ClientEndpointAnnotated cea;
+
+    public Control() {
+        try {
+            ClientManager client = ClientManager.createClient();
+            this.rec = new RecursoExpediente_Client();
+            this.cea = new ClientEndpointAnnotated();
+            client.connectToServer(cea, new URI(URI));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Expediente getExpediente(String id) {
+        rec.getExpediente(Expediente.class, id);
+        cea.sendMessage(id);
+        // Cea recibe json y luego será transformado a Expediente
+        return null;
+    }
+
+    public void putExpediente(Expediente expediente) {
+        rec.putExpediente(expediente);
+    }
+}
